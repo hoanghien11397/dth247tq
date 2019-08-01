1e0e1d5aa1e1aab8e5e67be9c3b65786c5146c42
phamthingocvan99 <ngocvannbk@gmail.com>
2019-08-01 10:05:08 +0700

    okkkk

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -37,7 +37,7 @@
         <div class="row align-items-center">
           
           <div class="col-6 col-xl-2">
-            <h1 class="mb-0 site-logo"><a href="index.html" class="text-black h2 mb-0"><img src="{{ asset('images/logo1.jpg')}}" style="width: 100%; height: 100%" alt=""></a></h1>
+            <h1 class="mb-0 site-logo"><a href="index.html" class="text-black h2 mb-0"><img src="{{ asset('images/logo1.jpg')}}" style="width: 80%; height: 80%" alt=""></a></h1>
           </div>
           <div class="col-12 col-md-10 d-none d-xl-block">
             <nav class="site-navigation position-relative text-right" role="navigation">
