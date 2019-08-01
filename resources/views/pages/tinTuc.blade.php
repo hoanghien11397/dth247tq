a0f8fed6b20a528dd7dde31cd07099f394c01932
hanhdoan1999 <doanhonghanh.1999@gmail.com>
2019-08-01 17:41:40 +0700

    tin tuc

---
+++ resources/views/pages/tinTuc.blade.php
@@ -0,0 +1,204 @@
+@extends('master-layout')
+
+
+@section('content')
+
+ <!-- ##### Tin Tuc  ##### -->
+ <section class="blog-area blog-page section-padding-100">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="blog-title text-center">
+                            <span class="blog-title-text">Tin Tức</span>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="250ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/News/Avatar/hoithaokhoahoc.png" alt="" >
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>Hội thảo "Sàn giao dịch công nghệ trực tuyến" </h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p> E-learning giải pháp đã giành Giải Nhất Nhân Tài Đất Việt cho giải pháp đào tạo trực tuyến</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="500ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/News/Avatar/nguyenthiennhan1%20251221595.jpg" alt="">
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>Trí Nam tham dự buổi khai mạc Hội nghị Quốc tế về Thành phố Thông minh 2017</h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p> E-learning giải pháp đã giành Giải Nhất Nhân Tài Đất Việt cho giải pháp đào tạo trực tuyến</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="750ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/News/Avatar/0903cacvichutri.jpg" alt="">
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>Trí Nam tham dự hội thảo "Mô hình hệ thống thông tin đất đai quốc gia"</h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p>Trí Nam tham dự hội thảo "Mô hình hệ thống thông tin đất đai quốc gia và lựa chọn phần mềm hệ thống"</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="250ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/Users/1/Images/2016.12.29/thu-tuong-2-nuoc-va-dai-dien-fpt-gti-tai-buoi-ky-ket.jpg" alt="">
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>ICT Press Club tổ chức tọa đàm "Doanh nghiệp ICT Việt vươn ra thế giới"</h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p> E-learning giải pháp đã giành Giải Nhất Nhân Tài Đất Việt cho giải pháp đào...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="500ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/Users/1/Images/2016.12.27/photo-0-1482826614248.jpg" alt="">
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>Bài học kinh doanh sâu sắc rút ra từ cuốn sách được cả Bill Gates và Warren Buffett khen là 'tuyệt vời nhất'</h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p> E-learning giải pháp đã giành Giải Nhất Nhân Tài Đất Việt cho giải pháp đào...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="750ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/Users/1/Images/2016.12.29/thu-tuong-2-nuoc-va-dai-dien-fpt-gti-tai-buoi-ky-ket.jpg" alt="">
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>Bài học kinh doanh sâu sắc rút ra từ cuốn sách được cả Bill Gates và Warren Buffett khen là 'tuyệt vời nhất'</h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p> E-learning giải pháp đã giành Giải Nhất Nhân Tài Đất Việt cho giải pháp đào...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="250ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/Users/1/Images/2016.12.29/thu-tuong-2-nuoc-va-dai-dien-fpt-gti-tai-buoi-ky-ket.jpg" alt="">
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>Bài học kinh doanh sâu sắc rút ra từ cuốn sách được cả Bill Gates và Warren Buffett khen là 'tuyệt vời nhất'</h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p> E-learning giải pháp đã giành Giải Nhất Nhân Tài Đất Việt cho giải pháp đào...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="500ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/Users/1/Images/2016.12.27/photo-0-1482826614248.jpg" alt="">
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>Bài học kinh doanh sâu sắc rút ra từ cuốn sách được cả Bill Gates và Warren Buffett khen là 'tuyệt vời nhất'</h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p> E-learning giải pháp đã giành Giải Nhất Nhân Tài Đất Việt cho giải pháp đào...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Blog Area -->
+                <div class="col-12 col-lg-4">
+                    <div class="single-blog-area mb-100 wow fadeInUp" data-wow-delay="750ms">
+                        <img style="height: 237px;" src="http://trinam.com.vn/Data/Portals/0/Users/1/Images/2016.12.29/thu-tuong-2-nuoc-va-dai-dien-fpt-gti-tai-buoi-ky-ket.jpg" alt="">
+                        <!-- Blog Content -->
+                        <div class="blog-content">
+                            <a href="#" class="blog-headline">
+                                <h5>Bài học kinh doanh sâu sắc rút ra từ cuốn sách được cả Bill Gates và Warren Buffett khen là 'tuyệt vời nhất'</h5>
+                            </a>
+                            <div class="meta d-flex align-items-center">
+                                <a href="#">09/18/2019</a>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <a href="#">Art &amp; Design</a>
+                            </div>
+                            <p> E-learning giải pháp đã giành Giải Nhất Nhân Tài Đất Việt cho giải pháp...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="load-more text-center mt-100 wow fadeInUp" data-wow-delay="900ms">
+                        <a href="#" class="btn btn-primary">Load More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+
+
+
+@endsection
