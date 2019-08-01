56ccbc0a9dc12fb55b6531359e988535caf0f7eb
phamthingocvan99 <ngocvannbk@gmail.com>
2019-08-01 19:34:58 +0700

    fghj

---
+++ resources/views/pages/dichvu.blade.php
@@ -0,0 +1,8 @@
+
+@extends('master-layout')
+
+@section('content')
+<div>
+    <h1>okkkk</h1>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::prefix('dichvu')->group(function () {
+
+    Route::get('/', function () {
+        return view('pages.dichvu');
+    })->name('dich-vu');
+    // Route::get('khuyen-mai-chi-tiet', function () {
+    //     return view('page.khuyenmaichitiet');
+    // })->name('khuyen-mai-chi-tiet');
+
+});
+
+
